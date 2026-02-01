6cdfa94e8cd8676b397b0565ee7adf44c5ace0de
LuoTianyi-arm64 <2153585992@qq.com>
2026-02-01 11:26:32 +0800

    bugging

---
+++ src/printrsrgb/__init__.py
@@ -0,0 +1,8 @@
+from .printrsrgb import printrgb
+import sys
+__all__ = ["printrgb"]
+__version__ = "0.1.1"
+
+if __name__ =='__main__':
+    printrgb("". join(map(str,sys.argv[1:])), rainbow = 1)
+    printrgb(sys.stdin.read(),rainbow = 1)
---
+++ test.py
@@ -0,0 +1,5 @@
+from printrsrgb import printrgb
+foreground_color = [102, 204, 255]
+background_color = (255, 211, 250)
+text = '天依最可爱了'
+printrgb(text, foreground_color=foreground_color, background_color=background_color)
